6bd0d5e21868ea511e33e815a77219c14cd3922c
Steve Hannah <steve@weblite.ca>
2020-05-07 09:06:29 -0700

    Added javadocs for TWTSideBarView

--- src/com/codename1/twitterui/views/TWTSideBarView.java
+++ src/com/codename1/twitterui/views/TWTSideBarView.java
@@ -50,16 +50,141 @@ import com.codename1.ui.plaf.Border;
 import java.io.IOException;
 
 /**
- *
+ * A component inspired by the Twitter mobile app's sidebar menu.  
+ * 
+ * image::https://shannah.github.io/TweetAppUIKit/manual/images/Image-050520-093228.115.png[]
+ * 
+ * This is designed to work seamlessly with the 
+ * {@link TWTFormController} and the {@link CollapsibleSideBarContainer} to provide a sidebar menu for 
+ * an app that uses the Tweet App UI Kit.  It is the default component returned by {@link TWTFormController#createSideVarView()}, and this controller
+ * uses the {@link CollapsibleSideBarContainer} view to wrap its content by default.
+ * 
+ * However, this component can be used on its own as well.
+ * 
+ * === View Model
+ * 
+ * There are no special requirements for the view model.  If the view model implements {@link Thing#name} and/or {@link Thing#thumbnailUrl},
+ * these will be used to generate the user avatar.   Customization of the content of this view is primarily handled
+ * by registering actions.
+ * 
+ * === Actions
+ * 
+ * . {@link #SIDEBAR_TOP_ACTIONS} - Actions displayed at the top of the sidebar.
+ * . {@link #SIDEBAR_TOP_OVERFLOW_MENU} - Actions displayed in the overflow menu at the top of the sidebar.
+ * . {@link #SIDEBAR_STATS} - Actions meant to display user stats (e.g. 2034 Posts, etc..).
+ * . {@link #SIDEBAR_ACTIONS} - The primary menu items in the sidebar.  These will render icon and text.
+ * . {@link #SIDEBAR_SETTINGS_ACTIONS} - Additional settings/related actions that are rendered below the {@link #SIDEBAR_ACTIONS} menu.  These will be rendered 
+ * as text only.  No icons.
+ * . {@link #SIDEBAR_BOTTOM_LEFT_ACTIONS} - Actions rendered in the bottom left.
+ * . {@link #SIDEBAR_BOTTOM_RIGHT_ACTIONS} - Actions rendered in the bottom right.
+ * 
+ * === Styles
+ * 
+ * . `TWTSideBar` - UIID for the component.
+ * . `TWTSideBarTopAction` - UIID used for buttons in the {@link #SIDEBAR_TOP_ACTIONS} category.
+ * . `TWTSideBarAction` - UIID used by main actions.  i.e. {@link #SIDEBAR_ACTIONS} category.
+ * . `TWTSideBarActionIcon` - UIID used for icons of main actions.  I.e. icons of {@link #SIDEBAR_ACTIONS} category.
+ * . `TWTSideBarStatsAction` - UIID used by stats actions.  I.e. {@link #SIDEBAR_STATS} category.
+ * . `TWTSideBarStatsActionIcon` - UIID used for icons of stats actions.
+ * . `TWTSideBarBottomLeftAction` - UIID used for bottom left actions. I.e. {@link #SIDEBAR_BOTTOM_LEFT_ACTIONS} category.
+ * . `TWTSideBarBottomLeftActionsWrapper` - UIID used for wrapper container of all of the bottom left actions.
+ * . `TWTSideBarBottomRightAction` - UIID used for bottom right actions. I.e. {@link #SIDEBAR_BOTTOM_RIGHT_ACTIONS} category.
+ * . `TWTSideBarBottomRightActionsWrapper` - UIID used for wrapper container of all of the bottom right actions.
+ * . `TWTSideBarWrapper` - Wrapper component inside `TWTSideBar` but wrapping all of the content.
+ * . `TWTSideBarNorth` - North container in the sidebar.  This wraps top actions, avatar/username, and stats actions.
+ * . `TWTSideBarOverflowMenuButton` - UIID of the overflow menu button.
+ * . `TWTSideBarNameLabel` - UIID for the label with the profile user name.
+ * . `TWTSideBarIDLabel` - UIID for the label with the user ID.
+ * . `TWTSideBarStatsActionsWrapper` - UIID for the wrapper container of all of the stats actions.
+ * . `TWTSideBarCenter` - UIID for the wrapper container of the CENTER slot in the TWTSideBarWrapper.  This wraps the SIDEBAR_ACTIONS and SIDEBAR_SETTINGS_ACTIONS.
+ * . `TWTSideBarActionsWrapper` - UIID for the sidebar actions wrapper container.
+ * . `TWTSideBarSettingsActionsWrapper` - UIID for the container that wraps the settings actions.
+ * . `TWTSideBarSouth` - UIID for the south container.  This wraps the bottom left and bottom right actions.
+ * 
+ * 
+ * === Examples
+ * 
+ * .Some basic example usage.  If you use the {@link TWTFormController} class for your form controller, then you wouldn't actually create the TWTSideBar view directly.  The controller will do that for you.  See next example for such a case.
+ * [source,java]
+ * ----
+ * public static final ActionNode profile = action(
+                label("Profile"),
+                icon(FontImage.MATERIAL_ACCOUNT_CIRCLE)
+        ),
+        lists = action(label("Lists"), icon(FontImage.MATERIAL_LIST)),
+        topics = action(label("Topics"), icon(FontImage.MATERIAL_CATEGORY)),
+        bookmarks = action(label("Bookmarks"), icon(FontImage.MATERIAL_BOOKMARKS)),
+        moments = action(label("Moments"), icon(FontImage.MATERIAL_BOLT)),
+        newAccount = action(label("Create new account")),
+        addExistingAccount = action(label("Add Existing Account")),
+        settingsAndPrivacy = action(label("Settings and privacy")),
+        helpCenter = action(label("Help Center")),
+        darkMode = action(icon(FontImage.MATERIAL_LIGHTBULB_OUTLINE)),
+        qrCode = action(icon(FontImage.MATERIAL_SCANNER)),
+        switchProfile = action(icon(FontImage.MATERIAL_ACCOUNT_CIRCLE)),
+        switchProfile2 = action(icon(FontImage.MATERIAL_ACCOUNT_BALANCE_WALLET)),
+        followers = action(
+                icon("Followers"),
+                label("344")
+        ),
+        following = action(
+            icon("Following"),
+            label("311")
+        ),
+   ...
+  
+ViewNode viewNode = new ViewNode( 
+            actions(TWTSideBarView.SIDEBAR_ACTIONS, profile, lists, topics, bookmarks, moments),
+            actions(TWTSideBarView.SIDEBAR_TOP_OVERFLOW_MENU, newAccount, addExistingAccount),
+            actions(TWTSideBarView.SIDEBAR_SETTINGS_ACTIONS, settingsAndPrivacy, helpCenter),
+            actions(TWTSideBarView.SIDEBAR_BOTTOM_LEFT_ACTIONS, darkMode),
+            actions(TWTSideBarView.SIDEBAR_BOTTOM_RIGHT_ACTIONS, qrCode),
+            actions(TWTSideBarView.SIDEBAR_TOP_ACTIONS, switchProfile, switchProfile2),
+            actions(TWTSideBarView.SIDEBAR_STATS, following, followers),
+            actions(TWTGlobalTabs.GLOBAL_TABS, home, search, alerts, messages),
+            actions(TWTTitleComponent.TITLE_ACTIONS, settings)
+                
+        );
+ 
+ TWTSideBarView view = new TWTSideBarView(myViewModel, viewNode)
+ * ----
  * @author shannah
  */
 public class TWTSideBarView extends AbstractEntityView {
+    /**
+     * Actions rendered at the top of the sidebar.
+     */
     public static final Category SIDEBAR_TOP_ACTIONS = new Category(),
+            
+            /**
+             * Actions displayed in the overflow menu at the top of the sidebar.
+             */
             SIDEBAR_TOP_OVERFLOW_MENU = new Category(),
+            
+            /**
+             * Actions meant to display user stats (e.g. 2034 Posts, etc..).
+             */
             SIDEBAR_STATS = new Category(),
+            
+            /**
+             * The primary menu items in the sidebar.  These will render icon and text.
+             */
             SIDEBAR_ACTIONS = new Category(),
+            
+            /**
+             * Additional settings/related actions that are rendered below the {@link #SIDEBAR_ACTIONS} menu.  These will be rendered 
+ * as text only.  No icons.
+             */
             SIDEBAR_SETTINGS_ACTIONS = new Category(),
+            
+            /**
+             * Actions rendered in the bottom left.
+             */
             SIDEBAR_BOTTOM_LEFT_ACTIONS = new Category(),
+            
+            /**
+             * Actions rendered in the bottom right.
+             */
             SIDEBAR_BOTTOM_RIGHT_ACTIONS = new Category();
             
             
@@ -72,13 +197,7 @@ public class TWTSideBarView extends AbstractEntityView {
     private static String fragmentTemplate;
     //private Container wrapper = new Container(new BorderLayout());
     private UIFragment fragment;
-    private Container avatarWrapper,
-            topActionsWrapper,
-            statsActionsWrapper,
-            sidebarActionsWrapper,
-            settingsActionsWrapper,
-            bottomLeftActionsWrapper,
-            bottomRightActionsWrapper;
+
     private static String loadFragmentTemplate() {
         if (fragmentTemplate == null) {
             try {
@@ -93,7 +212,12 @@ public class TWTSideBarView extends AbstractEntityView {
     }
     
     
-    
+   
+    /**
+     * Creates a new sidebar.
+     * @param entity The view model.  See class documentation for specific view model requirements.
+     * @param node The UI descriptor.  See class documentation for UI descriptor requirements, including supported action categories.
+     */
     public TWTSideBarView(Entity entity, ViewNode node) {
         super(entity);
         this.node = node;
